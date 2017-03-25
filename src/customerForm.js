bf1aa840ffd9d29b30735357d9e90b2df180eb5e
MacKenzie Judd <MacKenzie@Ambers-MacBook-Air.local>
2017-03-25 12:37:14 -0700

    wired up require

--- src/customerForm.js
+++ src/customerForm.js
@@ -1,15 +1,15 @@
 define(['angular'], (angular) => {
-  const customerForm = angular.module('customerForm', []);
-
-  const customerFormController = ['$http', ($http) => {
-    this.save = () => $http.post('api/customers', {
-      name: this.userForm.name,
-      email: this.userForm.email,
-      company: this.userForm.company,
-    })
-    .success(data => data)
-    .failure((data) => {
-      this.failureMessage = data;
-    });
-  }];
+  angular
+    .module('customerForm', []);
+    .controller('customerFormController', ['$http', ($http) => {
+      this.save = () => $http.post('api/customers', {
+        name: this.userForm.name,
+        email: this.userForm.email,
+        company: this.userForm.company,
+      })
+      .success(data => data)
+      .failure((data) => {
+        this.failureMessage = data;
+      });
+    }]);
 });
